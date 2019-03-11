da00ea0b2a536e5afc800d1cd60d0173607550ce
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 05:03:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,9 +34,9 @@ end
 
 def run(songs)
   help
-  puts "Please enter a command:"
-  command = gets.chomp
   until command == "exit"
+    puts "Please enter a command:"
+    command = gets.chomp
     if command == "help"
       help
     elsif command == "list"
